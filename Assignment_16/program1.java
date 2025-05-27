3122c6c1d56157ea19b6275badaecfad850bd905
sanketkatrajkar <135363208+sanketkatrajkar@users.noreply.github.com>
2025-05-28 03:02:13 +0530

    Add files via upload

---
+++ Assignment_16/program1.java
@@ -0,0 +1,72 @@
+///////////////////////////////////////////////////////////////////////////////
+// File Name   :  program2.java 
+// Description :  Accept the Rows and columns from the user and display the given pattern.
+// Author      :  Sanket Ashok Katrajkar
+// Date        :  26/05/2025
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+    Input : iRow = 5, iCol = 5
+    Output:  *     
+             *  *  
+             *  *  *  
+             *  *  *  * 
+        
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow , int iCol)
+    {
+         int i = 0, j = 0;
+
+         if(iRow != iCol)
+        {
+            System.out.println("Invalid input");
+            return;
+        }
+
+         for(i = 1; i <= iRow; i++)
+         {
+            for(j = 1; j <= iCol; j++)
+            {
+               if(j > i)  
+               {
+                    System.out.print(" \t");
+               }
+               else
+               {
+                  System.out.print("* \t");
+               }
+               
+            }
+
+            System.out.println();
+         }
+    }
+
+}
+
+
+public class program1
+{
+    public static void main(String args[])
+    {
+        int iValue1 = 0, iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter Number of Rows and columns :");
+
+        iValue1 = sobj.nextInt();
+        iValue2 = sobj.nextInt();
+        
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+    }   
+}
+
